9faa4e09665643bc474cd53475407de62ee122f2
Nicolas Lacombe <n.lacombe@oasiscommunication.ca>
2020-01-30 16:59:17 -0500

    Added Template library

---
+++ application/libraries/Template.php
@@ -0,0 +1,37 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_Loader
+ *
+ * @author formation
+ */
+
+class Template {
+
+  var $ci;
+
+  function __construct(){
+    $this->ci =& get_instance();
+}
+
+  public function load($view, $data = array(), $return = FALSE){
+
+    $this->ci->load->view("template/head",$data,$return);
+    $this->ci->load->view("template/header",$data, $return);
+    $this->ci->load->view($view,$data,$return);
+    $this->ci->load->view("template/footer",$data, $return);
+
+  }
+
+}
+
+
+
+?>
