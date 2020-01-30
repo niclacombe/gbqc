baffd10ac68f963136682a10c161829d015d7530
Nicolas Lacombe <n.lacombe@oasiscommunication.ca>
2020-01-30 16:57:27 -0500

    First

---
+++ application/views/template/footer.php
@@ -0,0 +1,13 @@
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?= site_url(); ?>assets/js/jquery.min.js"></script>
+  <script src="<?= site_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="<?= site_url(); ?>assets/js/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="<?= site_url(); ?>assets/js/resume.min.js"></script>
+
+</body>
+
+</html>
